c22a5e8fa07296ec20173abae6c91acb33440c6b
Shivam Sharma <shivam3246@gmail.com>
2021-12-10 14:52:25 +0530

    Updates

---
+++ precache-manifest.d8286fee0fc52a3f297d78ce387d20dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99027b35fe420da515388e39711cc808",
+    "url": "/TextUtility/index.html"
+  },
+  {
+    "revision": "5360bef31396f88c4db0",
+    "url": "/TextUtility/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "369d50299756284b6ec5",
+    "url": "/TextUtility/static/js/2.2b110539.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/TextUtility/static/js/2.2b110539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb0a3b6964fb93cacb71",
+    "url": "/TextUtility/static/js/3.42ad0d3d.chunk.js"
+  },
+  {
+    "revision": "5360bef31396f88c4db0",
+    "url": "/TextUtility/static/js/main.caee5c50.chunk.js"
+  },
+  {
+    "revision": "1ce60d5735bcd4ab5d89",
+    "url": "/TextUtility/static/js/runtime-main.05ea29f3.js"
+  }
+]);
